74d46306e9929e7022485d08373e39a00696d4fd
praveen kumar <pks101295@gmail.com>
2023-06-24 15:00:44 +0530

    html files added

--- deployment/main.py
+++ deployment/main.py
@@ -1,7 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 
-#initialize an app
+# initialize an app
 app = Flask(__name__)
 
-#run
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+   return render_template('contactus.html')
+
+
+@app.route('/courses')
+def courses():
+    return render_template('courses.html')
+
+
+@app.route('/images')
+def images():
+    return render_template('images.html')
+
+
+# run
 app.run()
